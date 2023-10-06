47fc8898ba9739134f9e3cbf0c1799ac86ff0df9
Emmanuel Andem <emmanuelandem360@gmail.com>
2023-10-06 21:30:11 +0100

    Fixed the bug on the favourites feature

--- src/contexts/FavouritesContext.jsx
+++ src/contexts/FavouritesContext.jsx
@@ -4,21 +4,21 @@ export const FavouritesContext = createContext();
 
 const FavouritesContextProvider = ({ children }) => {
   const [favourites, setFavourites] = useState(
-    localStorage.getItem("favourites") || []
+    JSON.parse(localStorage.getItem("favourites")) || []
   );
 
   const addToFavourites = (recipe) => {
     setFavourites((prevState) => {
       let newState = [...prevState];
       newState.push(recipe);
-      localStorage.setItem("favourites", newState);
+      localStorage.setItem("favourites", JSON.stringify(newState));
       return newState;
     });
   };
   const removeFromFavourites = (recipe) => {
     setFavourites((prevState) => {
       let newState = prevState.filter((item) => item.id != recipe.id);
-      localStorage.setItem("favourites", newState);
+      localStorage.setItem("favourites", JSON.stringify(newState));
       return newState;
     });
   };
